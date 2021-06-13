943f10e0ab1ae21ea00bf9418482815aa7e84bd8
Colin Eberhardt <colin.eberhardt@gmail.com>
2021-06-13 09:17:24 +0100

    move to const / let

--- prototype/kdb.js
+++ prototype/kdb.js
@@ -1,5 +1,5 @@
-var nodeq = require("node-q");
-var promisify = require('util').promisify;
+const nodeq = require("node-q");
+const promisify = require('util').promisify;
 
 // not sure why util.promisify doesn't work here?
 const qSend = (conn, value) => new Promise((resolve, reject) => {
@@ -9,11 +9,11 @@ const qSend = (conn, value) => new Promise((resolve, reject) => {
     });
 })
 
-var input=document.getElementById("txtInput");
-var output=document.getElementById("txtOutput");
-var result=document.getElementById("txtResult");
+const input=document.getElementById("txtInput");
+const output=document.getElementById("txtOutput");
+const result=document.getElementById("txtResult");
 
-var conn;
+let conn;
 
 async function connect() {
     conn = await promisify(nodeq.connect)({host: "localhost", port: 5005});
@@ -23,7 +23,7 @@ async function send() {
     const data = await qSend(conn, input.value);
     output.innerHTML = JSON.stringify(data, null, 2);
 
-    var outputHTML;
+    let outputHTML;
         
     if (data !== null) {
         if (typeof data == "object") {
@@ -35,7 +35,6 @@ async function send() {
                         data:data,
                         autoColumns:true
                     });
-
                 }
                 else {
                     outputHTML = generateTableHTML(data);
@@ -46,7 +45,7 @@ async function send() {
                     in this case we will iterate over the keys to print 
                     the key|value pairs as would be displayed in a q console
                 */
-                for (var x in data) {
+                for (let x in data) {
                     outputHTML += x + " | " + data[x] + "<br />";
                 }
             }
@@ -64,26 +63,14 @@ async function send() {
 
 function generateTableHTML(data){
     /* we will iterate through the object wrapping it in the HTML table tags */
-    var tableHTML = '<table border="1"><tr>';
+    let tableHTML = '<table border="1"><tr>';
     if (typeof data[0] == "object") {
-        for (var x in data[0]) {
-            /* loop through the keys to create the table headers */
-            tableHTML += '<th>' + x + '</th>';
-        }
+        tableHTML += data[0].map(x => `<th>${x}</th>`);
         tableHTML += '</tr>';
-        for (var i = 0; i < data.length; i++) {
-            /* loop through the rows, putting tags around each col value */
-            tableHTML += '<tr>';
-            for (var x in data[0]) {
-                tableHTML += '<td>' + data[i][x] + '</td>';
-            }
-            tableHTML += '</tr>';
-        }
+        tableHTML += data.map(row => data[0].map(col => `<td>${row[col]}</td>`));
     }
     else {
-        data.forEach((elem) => {
-            tableHTML += '<td>' + elem + '</td>';
-        })
+        tableHTML += data.map(x => `<td>${x}</td>`);
     }
     tableHTML += '</table>';
     return tableHTML;
