3963a2e08451e7e50c335cf3e4a25a3b3249d873
Colin Eberhardt <colin.eberhardt@gmail.com>
2021-06-14 12:13:08 +0100

    added the monaco editor

---
+++ prototype/editor.js
@@ -0,0 +1,35 @@
+// editor bootstrap, taken from:
+// https://github.com/microsoft/monaco-editor-samples/tree/main/electron-amd-nodeIntegration
+
+const path = require("path");
+const amdLoader = require("./node_modules/monaco-editor/min/vs/loader.js");
+const amdRequire = amdLoader.require;
+
+function uriFromPath(_path) {
+  var pathName = path.resolve(_path).replace(/\\/g, "/");
+  if (pathName.length > 0 && pathName.charAt(0) !== "/") {
+    pathName = "/" + pathName;
+  }
+  return encodeURI("file://" + pathName);
+}
+
+amdRequire.config({
+  baseUrl: uriFromPath(
+    path.join(__dirname, "./node_modules/monaco-editor/min")
+  ),
+});
+
+// workaround monaco-css not understanding the environment
+// self.module = undefined;
+
+module.exports = new Promise((resolve) => {
+  amdRequire(["vs/editor/editor.main"], () => {
+    const editor = monaco.editor.create(document.getElementById("txtInput"), {
+      value: "4 + 4",
+      minimap: {
+        enabled: false,
+      },
+    });
+    resolve(editor);
+  });
+});
