8a8f1aac9f6ecebbc3876f769e23730722540cf5
Ian Hull <ihull@scottlogic.com>
2021-07-12 14:09:52 +0100

    Format date and time columns appropriately

--- components/query-results.js
+++ components/query-results.js
@@ -1,5 +1,99 @@
 const { autoUpdater } = require("electron");
 
+const formatData = (data) => {
+  // Q has different types for date, time and datetime but these are all converted to JS Date
+  // object by node-q.
+  // The only way to know if a datetime is a date or a time is to check if time is zero (=> date)
+  // or date is 1 Jan 2000 (=> time, unless the date *really is* 1 Jan 2000....) - so apply
+  // some heuristics to work out how to format each column based on the data it contains.
+  // see https://www.npmjs.com/package/node-q#types
+
+  const _getDtType = (v) => {
+    if (v instanceof Date) {
+      if (
+        v.getHours() === 0 &&
+        v.getMinutes() === 0 &&
+        v.getSeconds() === 0 &&
+        v.getMilliseconds() === 0
+      ) {
+        return "date";
+      } else if (
+        v.getFullYear() === 2000 &&
+        v.getMonth() === 0 &&
+        v.getDate() === 1
+      ) {
+        if (v.getMilliseconds() > 0) {
+          return "timefull";
+        } else {
+          return "time";
+        }
+      } else {
+        return "datetime";
+      }
+    } else {
+      return "none";
+    }
+  };
+
+  const _formatDt = (val, dtType) => {
+    if (val instanceof Date) {
+      const dtISOStr = val.toISOString();
+      const [date, timeFull] = dtISOStr.split("T");
+      const [time, millisec] = timeFull.split(".");
+
+      switch (dtType) {
+        case "time":
+          return time;
+          break;
+        case "timefull":
+          return `${timeFull.replace(/Z$/, "")}`;
+        case "date":
+          return date;
+          break;
+        default:
+          return `${date} ${time}`;
+          break;
+      }
+    } else {
+      return val;
+    }
+  };
+
+  if (data instanceof Array) {
+    const colnames = Object.keys(data[0]);
+    let colDtType = {};
+
+    // For each column, if a Date object, establish by empirical means if it contains date, time or datetime
+    colnames.forEach((c) => {
+      let colDtTypeCount = {};
+      colDtTypeCount = data
+        .map((r) => r[c])
+        .reduce(
+          (acc, val) => {
+            acc[_getDtType(val)]++;
+            return acc;
+          },
+          { date: 0, time: 0, timefull: 0, datetime: 0, none: 0 }
+        );
+
+      // Get largest count and assume that's the datetime type for this column
+      colDtType[c] = Object.keys(colDtTypeCount).reduce((a, b) =>
+        colDtTypeCount[a] > colDtTypeCount[b] ? a : b
+      );
+    });
+
+    // Now reformat dates as strings, in-place
+    data.forEach((r) => {
+      colnames.forEach((c) => {
+        if (colDtType[c] !== "none") {
+          r[c] = _formatDt(r[c], colDtType[c]);
+        }
+      });
+    });
+  }
+  return data;
+};
+
 const queryResults = {
   props: {
     result: {
@@ -21,7 +115,7 @@ const queryResults = {
       columns: [],
       tableData: [],
 
-      tabHeight: 62, //TODO: work this out
+      tabHeight: 58, //TODO: work this out
     };
   },
 
@@ -53,7 +147,7 @@ const queryResults = {
                 // outputHTML = generateTableHTML(data);
                 console.log("About to set table cols/data");
                 const colnames = Object.keys(data[0]);
-                this.tableData = data;
+                this.tableData = formatData(data);
                 this.columns = colnames.map((c) => {
                   return {
                     prop: c,
