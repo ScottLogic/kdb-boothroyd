495f56731e80d9e9269915408e8f921ee1dfa0f2
Colin Eberhardt <colin.eberhardt@gmail.com>
2021-08-03 08:37:27 +0100

    added a utils file

--- src/components/server/ServerManager.tsx
+++ src/components/server/ServerManager.tsx
@@ -13,18 +13,11 @@ import ServerEdit from "./ServerEdit";
 import ServerDeleteConfirmation from "./ServerDeleteConfirmation";
 import ServerConnectionError from "./ServerConnectionError";
 import { stackTokens, serverPanel } from "../../style";
+import { removeAtIndex, replaceAtIndex} from "../../utils";
 
 type ServerManagerProps = {
   onConnect: (server: Server) => Promise<void>;
 };
-
-function replaceAtIndex<T>(array: T[], item: T, index: number) {
-  return Object.assign([], array, { [index]: item });
-}
-
-function removeAtIndex<T>(array: T[], index: number) {
-  return array.filter((_, i) => i !== index);
-}
 
 const ServerManager: FC<ServerManagerProps> = ({ onConnect }) => {
   const [servers, setServers] = useState<Server[]>([]);
