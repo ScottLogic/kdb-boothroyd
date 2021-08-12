b52e8b02e51f13bdbc0a7f2f728d6310c0a4fd9a
Will McKenzie <willmckenzie@oinutter.co.uk>
2021-08-12 10:04:17 +0100

    Add Edit Server tests

--- tests/server-management.js
+++ tests/server-management.js
@@ -163,6 +163,9 @@ describe ("Server Management", function () {
       assert.strictEqual(savedFiles.length, 3)
   
     })
+
+  })  
+
   describe("Delete Server", function () {
 
     it("should ask me if to confirm deletion of a server", async function () {
@@ -256,6 +259,97 @@ describe ("Server Management", function () {
     })
 
   })
+
+  describe("Edit Server", function () {
+
+    it ("should display the correct details if I select a server", async function () {
+
+      const serverToEdit = await this.modal.$(":nth-match(.server-list li, 2)")
+      await serverToEdit.click()
+      await this.appWindow.waitForTimeout(100)
+
+      const serverTab = await this.modal.$(".edit-tabs button:has-text('Server')")
+      await serverTab.click()
+      await this.appWindow.waitForTimeout(100)
+  
+      const nameField = await this.modal.$('.server-tab .name-input input')
+      const hostField = await this.modal.$('.server-tab .host-input input')
+      const portField = await this.modal.$('.server-tab .port-input input')
+      const tlsCheck = await this.modal.$('.server-tab .tls-check input')
+      const saveButton = await this.modal.$('button:has-text("Save")')
+      
+      assert.strictEqual(await nameField.inputValue(), "Test 1", "Name field is not correct")
+      assert.strictEqual(await hostField.inputValue(), "0.0.0.0", "Host field is not correct")
+      assert.strictEqual(await portField.inputValue(), "5001", "Port field is not correct")
+      assert.strictEqual(await tlsCheck.isChecked(), false, "TLS checkbox is not correct")
+
+      assert.strictEqual(await saveButton.getAttribute("aria-disabled"), "true", "Save button is not disabled")
+
+    })
+
+    it ("should let me update the details", async function () {
+      this.timeout(5000)
+
+      const serverToEdit = await this.modal.$(":nth-match(.server-list li, 2)")
+      await serverToEdit.click()
+      await this.appWindow.waitForTimeout(100)
+
+      const serverTab = await this.modal.$(".edit-tabs button:has-text('Server')")
+      await serverTab.click()
+      await this.appWindow.waitForTimeout(100)
+  
+      let nameField = await this.modal.$('.server-tab .name-input input')
+      let hostField = await this.modal.$('.server-tab .host-input input')
+      let portField = await this.modal.$('.server-tab .port-input input')
+      let tlsLabel = await this.modal.$('.server-tab .tls-check label')
+      let tlsInput = await this.modal.$('.server-tab .tls-check input')
+      let saveButton = await this.modal.$('button:has-text("Save")')
+      
+      assert.strictEqual(await nameField.inputValue(), "Test 1", "Name field is not correct")
+      assert.strictEqual(await hostField.inputValue(), "0.0.0.0", "Host field is not correct")
+      assert.strictEqual(await portField.inputValue(), "5001", "Port field is not correct")
+      assert.strictEqual(await tlsInput.isChecked(), false, "TLS checkbox is not correct")
+      assert.strictEqual(await saveButton.getAttribute("aria-disabled"), "true", "Save button is not disabled")
+
+      await nameField.selectText()
+      await nameField.type("Test Server")
+      await hostField.selectText()
+      await hostField.type("127.0.0.1")
+      await portField.selectText()
+      await portField.type("5002")
+      await tlsLabel.click()
+
+      assert.strictEqual(await nameField.inputValue(), "Test Server", "Name field is not correct")
+      assert.strictEqual(await hostField.inputValue(), "127.0.0.1", "Host field is not correct")
+      assert.strictEqual(await portField.inputValue(), "5002", "Port field is not correct")
+      assert.strictEqual(await tlsInput.isChecked(), true, "TLS checkbox is not correct 1")
+      assert.strictEqual(await saveButton.getAttribute("aria-disabled"), null, "Save button is still disabled")
+
+      await saveButton.click()
+      await this.appWindow.waitForTimeout(100)
+
+      const servers = await this.modal.$$(".server-list li")
+      
+      // Click off then click back to this server to make sure new details persist
+      await servers[0].click()
+      await this.appWindow.waitForTimeout(100)
+      await servers[1].click()
+      await this.appWindow.waitForTimeout(100)
+
+      nameField = await this.modal.$('.server-tab .name-input input')
+      hostField = await this.modal.$('.server-tab .host-input input')
+      portField = await this.modal.$('.server-tab .port-input input')
+      tlsInput = await this.modal.$('.server-tab .tls-check input')
+      saveButton = await this.modal.$('button:has-text("Save")')
+
+      assert.strictEqual(await nameField.inputValue(), "Test Server", "Name field is not correct")
+      assert.strictEqual(await hostField.inputValue(), "127.0.0.1", "Host field is not correct")
+      assert.strictEqual(await portField.inputValue(), "5002", "Port field is not correct")
+      assert.strictEqual(await tlsInput.isChecked(), true, "TLS checkbox is not correct 2")
+      assert.strictEqual(await saveButton.getAttribute("aria-disabled"), "true", "Save button is not disabled")
+
+    })
+
   })
   
 })
