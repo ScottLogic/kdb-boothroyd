7bd98979b8b29d7d6b3295451d0a996f34928c25
Will McKenzie <willmckenzie@oinutter.co.uk>
2021-08-12 08:13:16 +0100

    Add tests for server deletion

--- tests/server-management.js
+++ tests/server-management.js
@@ -156,6 +156,96 @@ describe ("Server Management", function () {
       assert.notStrictEqual(link, null)
   
     })
+  describe("Delete Server", function () {
+
+    it("should ask me if to confirm deletion of a server", async function () {
+
+      const serverToDelete = await this.modal.$(":nth-match(.server-list li, 3)")
+
+      await serverToDelete.click()
+      await this.appWindow.waitForTimeout(100)
+
+      const deleteButton = await this.modal.$("button:has-text('Delete')")
+      await deleteButton.click()
+      await this.appWindow.waitForTimeout(100)
+
+      const deleteConfirmation = await this.appWindow.$('.server-delete-confirmation')
+
+      assert.notStrictEqual(deleteConfirmation, null)
+
+      const title = await deleteConfirmation.$('.ms-Dialog-title')
+      const content = await deleteConfirmation.$('p.ms-Dialog-subtext')
+
+      assert.strictEqual(await title.innerText(), "Delete Server")
+      assert.strictEqual(await content.innerText(), "Are you sure you want to delete this server?")
+
+      const confirm = await deleteConfirmation.$('.ms-Button--primary:has-text("Delete")')
+      const cancel = await deleteConfirmation.$('.ms-Button--default:has-text("Cancel")')
+
+      assert.notStrictEqual(confirm, null)
+      assert.notStrictEqual(cancel, null)
+
+      await cancel.click()
+    })
+
+    it("should not delete if I cancel", async function () {
+
+      const serverToDelete = await this.modal.$(":nth-match(.server-list li, 3)")
+
+      await serverToDelete.click()
+      await this.appWindow.waitForTimeout(100)
+
+      const deleteButton = await this.modal.$("button:has-text('Delete')")
+      await deleteButton.click()
+      await this.appWindow.waitForTimeout(100)
+
+      let deleteConfirmation = await this.appWindow.$('.server-delete-confirmation')
+      
+      const cancel = await deleteConfirmation.$('button.ms-Button--default:has-text("Cancel")')
+      await cancel.click()
+      await this.appWindow.waitForTimeout(500)
+      
+      deleteConfirmation = await this.appWindow.$('.server-delete-confirmation')
+      assert.strictEqual(deleteConfirmation, null)
+
+      const servers = await this.modal.$$(".server-list li")
+          
+      assert.strictEqual(servers.length, 3, "Incorrect number of servers")
+      assert.strictEqual(await servers[0].innerText(), "Localhost")
+      assert.strictEqual(await servers[1].innerText(), "Test 1")
+      assert.strictEqual(await servers[2].innerText(), "Test 2")
+
+    })
+
+    it("should delete if I confirm", async function () {
+
+      const serverToDelete = await this.modal.$(":nth-match(.server-list li, 3)")
+
+      await serverToDelete.click()
+      await this.appWindow.waitForTimeout(100)
+
+      const deleteButton = await this.modal.$("button:has-text('Delete')")
+      await deleteButton.click()
+      await this.appWindow.waitForTimeout(100)
+
+      let deleteConfirmation = await this.appWindow.$('.server-delete-confirmation')
+      
+      const confirm = await deleteConfirmation.$('button.ms-Button--primary:has-text("Delete")')
+      await confirm.click()
+      await this.appWindow.waitForTimeout(500)
+      
+      deleteConfirmation = await this.appWindow.$('.server-delete-confirmation')
+      assert.strictEqual(deleteConfirmation, null)
+
+      const servers = await this.modal.$$(".server-list li")
+          
+      assert.strictEqual(servers.length, 2, "Server not deleted")
+      assert.strictEqual(await servers[0].innerText(), "Localhost")
+      assert.strictEqual(await servers[1].innerText(), "Test 1")
+
+    })
+
+  })
   })
   
 })
