e261c76eac4e1b329e3f969e5ba2d3ceeea5cd93
Will McKenzie <willmckenzie@oinutter.co.uk>
2021-09-08 13:11:17 +0100

    ci: Add tests for handling multiple connection tabs
    Check that Pivot overflow mechanism triggers if too many connection tabs are opened.

--- tests/server-connection.js
+++ tests/server-connection.js
@@ -97,4 +97,46 @@ describe("Server Connection", () => {
       "Connection tab is labelled incorrectly"
     );
   });
+
+  it("should truncate the tab list if there are too many connections", async function () {
+    this.timeout(10000);
+    const serversButton = await this.appWindow.$(".show-servers-button");
+
+    for (let i = 0; i < 7; i++) {
+      await serversButton.click();
+      await this.appWindow.waitForTimeout(100);
+
+      this.modal = await this.appWindow.$(".server-management-modal");
+
+      assert.notStrictEqual(this.modal, null, "Server modal has not appeared");
+
+      const server = await this.modal.$(":nth-match(.server-list li, 1)");
+      await server.click();
+      await this.appWindow.waitForTimeout(100);
+
+      const connectButton = await this.modal.$('button:has-text("Connect")');
+
+      await connectButton.click();
+      await this.modal.waitForElementState("hidden");
+    }
+
+    const overflowButton = await this.appWindow.waitForSelector(
+      ".connection-tabs .ms-Pivot-overflowMenuButton"
+    );
+
+    assert.notStrictEqual(overflowButton, null);
+    assert.strictEqual(await overflowButton.isVisible(), true);
+
+    await overflowButton.click();
+
+    const callout = await this.appWindow.waitForSelector(
+      ".ms-Callout.ms-ContextualMenu-callout"
+    );
+
+    assert.strictEqual(await callout.isVisible(), true);
+
+    const items = await callout.$$("li.ms-ContextualMenu-item");
+
+    assert.strictEqual(items.length > 0, true);
+  });
 });
